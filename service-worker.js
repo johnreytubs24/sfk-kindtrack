f2b40990f22b7f2bbaf406221c6eb279191bf2d1
SFK <109145884+SFK2627@users.noreply.github.com>
2026-07-27 19:49:07 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sfk-kindtrack-portrait35-monthdate-v27';
+const CACHE_NAME = 'sfk-kindtrack-desktop-extra-075in-side-panels-v30';
 const APP_SHELL = [
   './',
   './index.html',
